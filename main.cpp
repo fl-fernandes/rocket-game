9349475de4b9c1c8ea780012436ebce770b4e143
angelMizutani <angelisayuri@yahoo.com.br>
2022-06-14 12:23:41 -0300

    the square is moving

--- main.cpp
+++ main.cpp
@@ -5,6 +5,37 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+//The rectangle that represents our rocket
+class Rocket
+{
+    public:
+        //Dimensions of the rocket
+        static const int ROCKET_WIDTH = 10;
+        static const int ROCKET_HEIGHT = 10;
+
+        //Maximum axis velocity of the rocket
+        static const int ROCKET_VEL = 1;
+
+        //Initializes the variables
+        Rocket();
+
+        //Takes key presses and adjusts the rocket's velocity
+        void handleEvent(SDL_Event& e);
+
+        //Moves the rocket
+        void move();
+
+        //Shows the rocket on the screen
+        void render();
+
+    private:
+        //The X and Y offsets of the dot
+        int mPosX, mPosY;
+
+        //The velocity of the dot
+        int mVelX, mVelY;
+};
+
 //Starts up SDL and creates window
 bool init();
 
@@ -19,6 +50,103 @@ SDL_Window* gWindow = NULL;
 
 //The window renderer
 SDL_Renderer* gRenderer = NULL;
+
+Rocket::Rocket()
+{
+    //Initialize the offsets
+    mPosX = 0;
+    mPosY = 0;
+
+    //Initialize the velocity
+    mVelX = 0;
+    mVelY = 0;
+}
+
+void Rocket::handleEvent(SDL_Event& e)
+{
+    //If a key is pressed
+    if (e.type == SDL_KEYDOWN && e.key.repeat == 0)
+    {
+        //adjust the velocity
+        switch (e.key.keysym.sym)
+        {
+        case SDLK_UP:
+            mVelY -= ROCKET_VEL;
+            break;
+        
+        case SDLK_DOWN:
+            mVelY += ROCKET_VEL;
+            break;
+
+        case SDLK_LEFT:
+            mVelX -= ROCKET_VEL;
+            break;
+        
+        case SDLK_RIGHT:
+            mVelX += ROCKET_VEL;
+            break;
+        
+        default:
+            break;
+        }
+    } //If a key was released 
+    else if (e.type == SDL_KEYUP && e.key.repeat == 0)
+    {
+        //Adjust the velocity
+        switch (e.key.keysym.sym)
+        {
+            case SDLK_UP:
+            mVelY += ROCKET_VEL;
+            break;
+        
+        case SDLK_DOWN:
+            mVelY -= ROCKET_VEL;
+            break;
+
+        case SDLK_LEFT:
+            mVelX += ROCKET_VEL;
+            break;
+        
+        case SDLK_RIGHT:
+            mVelX -= ROCKET_VEL;
+            break;
+        
+        default:
+            break;
+        }
+    }
+}
+
+void Rocket::move()
+{
+    //move the rocket left or right
+    mPosX += mVelX;
+
+    //If the rocket went too far to the left or right
+    if ((mPosX < 0) || (mPosX + ROCKET_WIDTH > SCREEN_WIDTH))
+    {
+        //move back
+        mPosX -= mVelX;
+    }
+
+    //move the rocket up or down
+    mPosY += mVelY;
+
+    //If the rocket went too far to the left or right
+    if ((mPosY < 0) || (mPosY + ROCKET_HEIGHT > SCREEN_HEIGHT))
+    {
+        //move back
+        mPosY -= mVelY;
+    }
+
+}
+
+void Rocket::render()
+{
+    SDL_Rect rect = {mPosX, mPosY, ROCKET_WIDTH, ROCKET_HEIGHT};
+    SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
+    SDL_RenderFillRect(gRenderer, &rect);
+}
 
 bool init()
 {
@@ -104,6 +232,9 @@ int main(int argc, char* args[])
             //Event handler
             SDL_Event e;
 
+            //The rocket that will be moving around the screen
+            Rocket rocket;
+
             //While application is running
             while(!quit)
             {
@@ -115,16 +246,20 @@ int main(int argc, char* args[])
                     {
                         quit = true;
                     }
+
+                    //Handle input for the rocket
+                    rocket.handleEvent(e);
                 }    
+
+                //move the rocket
+                rocket.move();  
 
                 //Clear screen
                 SDL_SetRenderDrawColor(gRenderer, 0, 0, 0, 0);   
                 SDL_RenderClear(gRenderer);
 
-                //Render red filled quad
-                SDL_Rect SDL_FillRect = {SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 10, 10};
-                SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
-                SDL_RenderFillRect(gRenderer, &SDL_FillRect);
+                //Render red filled rectangle (which will be our rocket in the future)
+                rocket.render();
 
                 //Update the screen
                  SDL_RenderPresent(gRenderer);
